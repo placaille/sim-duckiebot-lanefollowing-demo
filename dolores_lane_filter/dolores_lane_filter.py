e038f1497b72751bd4f2c464176147855765b992
Philippe Lacaille <philippe.lacaille@gmail.com>
2018-11-08 17:16:38 -0500

    Create package for Dolores + make links files to work

--- dolores_lane_filter/dolores_lane_filter.py
+++ dolores_lane_filter/dolores_lane_filter.py
@@ -6,10 +6,10 @@ import numpy as np
 from duckietown_msgs.msg import WheelsCmdStamped
 
 def continuous_publisher():
-    vel_pub = rospy.Publisher('/random/topic/', WheelsCmdStamped, queue_size=1)
-    rospy.init_node('continuous_test_publisher')
+    vel_pub = rospy.Publisher('/random/dolores_special/', WheelsCmdStamped, queue_size=1)
+    rospy.init_node('dolores_lane_filter')
     rate = rospy.Rate(10)
-    
+
     while not rospy.is_shutdown():
         msg = WheelsCmdStamped()
         msg.vel_left = np.random.random()
@@ -21,5 +21,3 @@ def continuous_publisher():
 
 if __name__ == '__main__':
     continuous_publisher()
-
-
